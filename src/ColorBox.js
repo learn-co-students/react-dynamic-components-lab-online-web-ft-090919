294f8db82dbe76fb0bd943a219fa4e375213591a
mpuchyr <matt.puchyr@gmail.com>
2020-01-20 17:56:40 -0600

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 export default class ColorBox extends Component {
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: 2}}>   //Note: The style attribute accepts a JavaScript object with camelCased properties rather than a CSS string
-        {/* your conditional code here! */}
+    let newOpacity = parseFloat((this.props.opacity - 0.1).toFixed(1))
+    
+
+    return this.props.opacity >= 0.2 ? (
+      <div className="color-box" style={{opacity: this.props.opacity}}> 
+        {}
+        <ColorBox opacity={newOpacity} />
       </div>
-    )
+    ) : null
   }
 
 }
